5d31c07c41f3aa37e767ee19413333c30cc85645
Amberlee <amberleec12@yahoo.com>
2017-05-03 16:22:50 -0500

    initial commit

--- JS/simon.js
+++ JS/simon.js
@@ -6,44 +6,5 @@
     "use strict";
 
 
-    var game = {
-        possibleSeq : ['blue', 'red', 'yellow', 'green'],
-        currentSeq : [],
-        level: 0
-    };
-
-    function newSeq(){
-        game.currentSeq.push(game.possibleSeq[(Math.floor(Math.random()*4))]);
-        console.log(game.currentSeq);
-    }
-
-
-    $('#beginGame').click(function () {
-        newGame();
-        newSeq();
-        animateSeq();
-    });
-
-    $('#blueButton').click(function () {
-        animateSeq('#blueButton');
-    });
-
-    $('#yellowButton').click(function () {
-        animateSeq('#yellowButton');
-    });
-
-    $('#greenButton').click(function () {
-        animateSeq('#greenButton');
-    });
-
-    $('#redButton').click(function () {
-        animateSeq('#redButton');
-    });
-
-    $('#tryAgain').click(function () {
-        $('#tryAgain').hide();
-        newSeq();
-        animateSeq();
-    });
 
 
