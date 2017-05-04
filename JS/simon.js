fb2cdb179840aedac9d7eed34a27da27005f7ff2
Amberlee <amberleec12@yahoo.com>
2017-05-04 14:44:06 -0500

    working on a different functionality to try and make the game work.

--- JS/simon.js
+++ JS/simon.js
@@ -15,25 +15,46 @@ function sequence () {
     litTiles.push(Math.floor(Math.random()*4));
     lightUp()
 }
-
-function lightUp() {
-
-}
-//     for (var i = 0; i < litTiles.length; i++) {
-//         id = "#" + litTiles[i];
-//         $(id).animate({
-//             opacity: 0.5
-//         }, litTiles[i] * 800).animate({
-//             opacity: 1
-//         }, 100)
-//     }
+//
+// function lightUp() {
+//
 // }
+// //     for (var i = 0; i < litTiles.length; i++) {
+// //         id = "#" + litTiles[i];
+// //         $(id).animate({
+// //             opacity: 0.5
+// //         }, litTiles[i] * 800).animate({
+// //             opacity: 1
+// //         }, 100)
+// //     }
+// // }
+//
+// $('.play').on("click", function() {
+//     // if(0
+//     sequence();
+//     setTimeout(lightUp(tiles), 1000);
+// })
 
-$('.play').on("click", function() {
-    // if(0
-    sequence();
-    setTimeout(lightUp(tiles), 1000);
-})
+    $('.colors').click(function () {
+        if ($(this).('id') === tiles.sequence[userSeq]) {
+            userSeq++;
+            if (userSeq === tiles.sequence.length) {
+                //$('#newRound').show();
+                userSeq = 0;
+                game.level++;
+                newSeq();
+                $('#btn').text("Level: " + (game.level + 1));
+            }
+        } else {
+            console.log('game over');
+            $('#btn').text("Try Again").css('font-size', '1.5rem');
+        }
+    });
 
+    $('#start').click(function () {
+        newGame();
+        newSeq();
+        $('#btn').text("Level: " + (game.level + 1))
+    });
 
 });
