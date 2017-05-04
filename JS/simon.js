976d5729888278af8ce85d52e40523c0bfd78699
Amberlee <amberleec12@yahoo.com>
2017-05-04 08:53:03 -0500

    initial commit

--- JS/simon.js
+++ JS/simon.js
@@ -5,6 +5,22 @@
 
     "use strict";
 
+var tiles = [1,2,3,4];
+var litTiles = [];
 
+function lightUp(arr){
+    for(let i=0; i< arr.length; i++){
+        let id ="#" + arr[i];
+        $(id).animate({
+            opacity: 0.5
+        },arr[i] * 800).animate({
+            opacity: 1
+        }, 100)
+    }
+}
+
+$('.play').on("click", function(){
+    setTimeout(lightUp(tiles), 1000);
+});
 
 
