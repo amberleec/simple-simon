424b330f37ce35cf1655902ade202b05cbb5273b
Amberlee <amberleec12@yahoo.com>
2017-05-04 11:41:48 -0500

    initial commit

--- JS/simon.js
+++ JS/simon.js
@@ -2,25 +2,38 @@
  * Created by Amberlee on 5/3/17.
  */
 
+$(document).ready(function () {
 
     "use strict";
 
 var tiles = [1,2,3,4];
 var litTiles = [];
+var id;
 
-function lightUp(arr){
-    for(let i=0; i< arr.length; i++){
-        let id ="#" + arr[i];
-        $(id).animate({
-            opacity: 0.5
-        },arr[i] * 800).animate({
-            opacity: 1
-        }, 100)
-    }
+
+function sequence () {
+    litTiles.push(Math.floor(Math.random()*4));
+    lightUp()
 }
 
-$('.play').on("click", function(){
+function lightUp() {
+
+}
+//     for (var i = 0; i < litTiles.length; i++) {
+//         id = "#" + litTiles[i];
+//         $(id).animate({
+//             opacity: 0.5
+//         }, litTiles[i] * 800).animate({
+//             opacity: 1
+//         }, 100)
+//     }
+// }
+
+$('.play').on("click", function() {
+    // if(0
+    sequence();
     setTimeout(lightUp(tiles), 1000);
+})
+
+
 });
-
-
